15e26cfaa178d42d08ddddc662209744313d3074
trojik <mudarisov@it-projects.info>
2018-12-05 19:27:42 +0500

    :fire: adding database creation check

--- saas_test/tests/test_saas_template.py
+++ saas_test/tests/test_saas_template.py
@@ -3,6 +3,7 @@
 
 
 from odoo.tests.common import TransactionCase, tagged
+from odoo.service import db
 
 
 @tagged('post_install', 'at_install')
@@ -13,13 +14,22 @@ class TestSaasTemplate(TransactionCase):
             'template_modules_domain': 'mail',
             'template_post_init': "self.env['mail.message'].create({'message_type': 'notification',}).message_format()"
         })
-        self.saas_operator = self.env['saas.operator'].create()
+        self.saas_operator = self.env['saas.operator'].create({})
         self.saas_template_operator = self.env['saas.template.operator'].create({
-            'template_id': self.saas_template,
-            'operator_id': self.saas_operator,
-            'db_name': 'test_db_name',
+            'template_id': self.saas_template.id,
+            'operator_id': self.saas_operator.id,
+            'operator_db_name': 'test_db_name',
+            'to_rebuild': True,
+            'state': 'draft'
         })
 
     def test_template_operator(self):
         self.saas_template_operator.preparing_template_next()
-        self.saas_template_operator.db_id.create_db()
+        self.saas_template_operator.operator_db_id.create_db(
+            None,
+            self.saas_template_operator.template_id.template_demo
+        )
+        self.assertTrue(self.saas_template_operator.operator_db_id.name)
+        self.assertEqual(self.saas_template_operator.operator_db_id.name, 'test_db_name')
+        self.assertIn('test_db_name', db.list_dbs())
+
