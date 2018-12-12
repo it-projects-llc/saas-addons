c132454f187870de703195d1ced9c2289c5a8421
Denis Mudarisov <mudarisov@it-projects.info>
2018-12-12 19:34:09 +0500

    :fire: expanding database creation tests

--- saas_test/tests/test_saas_template.py
+++ saas_test/tests/test_saas_template.py
@@ -5,26 +5,28 @@
 from odoo.tests.common import TransactionCase, tagged
 from odoo.service import db
 
-
 @tagged('post_install', 'at_install')
 class TestSaasTemplate(TransactionCase):
     def setUp(self):
         super(TestSaasTemplate, self).setUp()
         self.saas_template = self.env['saas.template'].create({
             'template_modules_domain': 'mail',
-            'template_post_init': "self.env['mail.message'].create({'message_type': 'notification',}).message_format()"
+            'template_post_init': "self.env['mail.message'].create({'message_type': 'notification',}).message_format()",
         })
-        self.saas_operator = self.env['saas.operator'].create({})
+        self.saas_operator = self.env['saas.operator'].create({
+            'type': 'local',
+        })
         self.saas_template_operator = self.env['saas.template.operator'].create({
             'template_id': self.saas_template.id,
             'operator_id': self.saas_operator.id,
             'operator_db_name': 'test_db_name',
-            'to_rebuild': True,
-            'state': 'draft'
         })
 
     def test_template_operator(self):
         self.saas_template_operator.preparing_template_next()
+        # when preparing_template_next is called, a database is created. To check create_db separately,
+        # we first need to delete the old one.
+        self.saas_template_operator.operator_db_id.drop_db()
         self.saas_template_operator.operator_db_id.create_db(
             None,
             self.saas_template_operator.template_id.template_demo
@@ -32,3 +34,4 @@ class TestSaasTemplate(TransactionCase):
         self.assertTrue(self.saas_template_operator.operator_db_id.name)
         self.assertEqual(self.saas_template_operator.operator_db_id.name, 'test_db_name')
         self.assertIn('test_db_name', db.list_dbs())
+        self.saas_template_operator.operator_db_id.drop_db()
