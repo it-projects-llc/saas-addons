5722622f83489c58d1b1b6522ef6961840d1ec37
Denis Mudarisov <mudarisov@it-projects.info>
2018-12-24 15:42:59 +0500

    :fire: add tests that passed code to template_post_init is works

--- saas_test/tests/test_saas_template.py
+++ saas_test/tests/test_saas_template.py
@@ -1,7 +1,6 @@
 # Copyright 2018 Denis Mudarisov <https://it-projects.info/team/trojikman>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-import logging
 
 import odoo
 from odoo import SUPERUSER_ID
@@ -9,40 +8,50 @@ from odoo.tests.common import TransactionCase, tagged
 from odoo.service import db
 from odoo.addons.queue_job.job import Job, ENQUEUED, PENDING
 
-_logger = logging.getLogger(__name__)
 DB_TEMPLATE = 'db_template'
 DB_INSTANCE = 'db_instance'
-
-def is_module_installed(db_name, module_name):
-    db = odoo.sql_db.db_connect(db_name)
-    odoo.registry(db_name).check_signaling()
-    with odoo.api.Environment.manage(), db.cursor() as cr:
-        try:
-            _logger.debug('%s: %s', db_name, module_name)
-            env = odoo.api.Environment(cr, SUPERUSER_ID, {})
-            return getattr(env['ir.module.module'], 'search')([('name', '=', module_name), ('state', '=', 'installed')])
-        except:
-            _logger.error('Error while searching module %s in %s', module_name, db_name)
+MODULES = 'mail'
+TEST_SUBJECT = 'Dummy subject name to test that code is applied'
 
 
 @tagged('post_install', 'at_install')
 class TestSaasTemplate(TransactionCase):
-    # TODO: add second optional argument - method_name
-    def perform_last_job(self, model_name):
-        job_id = self.env['queue.job'].search([('model_name', '=', model_name)])
+    def perform_last_job(self, model_name, method_name):
+        job_id = self.env['queue.job'].search(
+            [('model_name', '=', model_name), ('method_name', '=', method_name)],
+            order='id DESC',
+            limit=1,
+        )
         if job_id:
-            job = Job.load(self.env, job_id[-1].uuid)
+            job = Job.load(self.env, job_id.uuid)
             if job.state == ENQUEUED or job.state == PENDING:
                 job.perform()
+
+    def assert_modules_is_installed(self, db_name, module_name):
+        db = odoo.sql_db.db_connect(db_name)
+        odoo.registry(db_name).check_signaling()
+        with odoo.api.Environment.manage(), db.cursor() as cr:
+            env = odoo.api.Environment(cr, SUPERUSER_ID, {})
+            return self.assertTrue(getattr(env['ir.module.module'],'search')
+                                       ([('name', '=', module_name), ('state', '=', 'installed')]))
+
+    def assert_record_is_created(self, db_name, model_name, search_domain):
+        db = odoo.sql_db.db_connect(db_name)
+        odoo.registry(db_name).check_signaling()
+        with odoo.api.Environment.manage(), db.cursor() as cr:
+            env = odoo.api.Environment(cr, SUPERUSER_ID, {})
+            return self.assertTrue(getattr(env[model_name], 'search')(search_domain))
 
     def setUp(self):
         super(TestSaasTemplate, self).setUp()
         self.saas_template = self.env['saas.template'].create({
-            'template_modules_domain': 'mail',
-            'template_post_init': "self.env['mail.message'].create({'message_type': 'notification',}).message_format()",
+            'template_modules_domain': MODULES,
+            'template_post_init': 'self.env[\'mail.message\'].create({\'subject\': \'' + TEST_SUBJECT + '\', })',
         })
+
         self.saas_operator = self.env['saas.operator'].create({
             'type': 'local',
+            'db_url_template': '{db_name}.{db_id}.test',
         })
         self.saas_template_operator = self.env['saas.template.operator'].create({
             'template_id': self.saas_template.id,
@@ -54,11 +63,11 @@ class TestSaasTemplate(TransactionCase):
         # FIXME: that check needed when last tests didn't pass, not sure that it is correct way to drop db
         if DB_TEMPLATE in db.list_dbs():
             db.exp_drop(DB_TEMPLATE)
-        self.saas_template_operator.preparing_template_next()
-        self.perform_last_job('saas.db')
-        self.perform_last_job('saas.template.operator')
-        self.perform_last_job('saas.template.operator')
 
+        self.saas_template_operator.preparing_template_next()
+        self.perform_last_job('saas.db', 'create_db')
+        self.perform_last_job('saas.template.operator', '_on_template_created')
+        self.perform_last_job('saas.template.operator', '_install_modules')
 
         # Tests that template db created correctly
         self.assertTrue(self.saas_template_operator.operator_db_id.name)
@@ -66,6 +75,9 @@ class TestSaasTemplate(TransactionCase):
         self.assertIn(DB_TEMPLATE, db.list_dbs())
 
         # Check that module from template_modules_domain is installed
-        self.assertTrue(is_module_installed(DB_TEMPLATE, 'mail'))
+        self.assert_modules_is_installed(DB_TEMPLATE, MODULES)
+        self.assert_record_is_created(DB_TEMPLATE, 'mail.message', [('subject', '=', TEST_SUBJECT)])
 
-        # self.saas_template_operator.create_db()
+        # TODO: check that there were no errors when creating the database
+
+        self.saas_template_operator.create_db()
