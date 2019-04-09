7c952273ddfda604d7b53eb0bb35d2e86c65de94
Denis Mudarisov <mudarisov@it-projects.info>
2019-04-09 18:03:17 +0500

    :fire: expand testing work with templates and builds

--- saas_test/tests/test_saas_template.py
+++ saas_test/tests/test_saas_template.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Denis Mudarisov <https://it-projects.info/team/trojikman>
+# Copyright 2018-2019 Denis Mudarisov <https://it-projects.info/team/trojikman>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
@@ -8,8 +8,10 @@ from odoo.tests.common import TransactionCase, tagged
 from odoo.service import db
 from odoo.tools.safe_eval import safe_eval
 
-DB_TEMPLATE = 'db_template'
-DB_INSTANCE = 'db_instance'
+DB_TEMPLATE_1 = 'db_template_1'
+DB_TEMPLATE_2 = 'db_template_2'
+DB_INSTANCE_1 = 'db_instance_1'
+DB_INSTANCE_2 = 'db_instance_2'
 MODULES = '[\'mail\']'
 TEMPLATE_TEST_SUBJECT = 'Dummy subject name to test that code is applied on template database'
 BUILD_TEST_SUBJECT = 'Dummy subject name to test that code is applied on build database'
@@ -51,49 +53,93 @@ class TestSaasTemplate(TransactionCase):
             test_queue_job_no_delay=True,
         ))
 
-        self.saas_template = self.env['saas.template'].create({
+        self.saas_template_1 = self.env['saas.template'].create({
             'template_modules_domain': MODULES,
             'template_post_init': 'env[\'mail.message\'].create({\'subject\': \'' + TEMPLATE_TEST_SUBJECT + '\', })',
             'build_post_init': 'env[\'{mail_message}\'].create({{\'subject\': \'' + BUILD_TEST_SUBJECT + '\', }})',
         })
 
-        self.saas_operator = self.env['saas.operator'].create({
+        self.saas_template_2 = self.env['saas.template'].create({
+            'template_modules_domain': MODULES,
+            'template_post_init': 'env[\'mail.message\'].create({\'subject\': \'' + TEMPLATE_TEST_SUBJECT + '\', })',
+        })
+
+        self.saas_operator_1 = self.env['saas.operator'].create({
             'type': 'local',
             'db_url_template': 'http://{db_name}.{db_id}.127.0.0.1.nip.io:8069',
         })
-        self.saas_template_operator = self.env['saas.template.operator'].create({
-            'template_id': self.saas_template.id,
-            'operator_id': self.saas_operator.id,
-            'operator_db_name': DB_TEMPLATE,
+
+        self.saas_operator_2 = self.env['saas.operator'].create({
+            'type': 'local',
+            'db_url_template': 'http://{db_name}.{db_id}.127.0.0.1.nip.io:8069',
         })
 
-        self.build_post_init_line = self.env['build.post_init.line'].create({
+        self.saas_template_operator_1 = self.env['saas.template.operator'].create({
+            'template_id': self.saas_template_1.id,
+            'operator_id': self.saas_operator_1.id,
+            'operator_db_name': DB_TEMPLATE_1,
+        })
+
+        self.saas_template_operator_2 = self.env['saas.template.operator'].create({
+            'template_id': self.saas_template_2.id,
+            'operator_id': self.saas_operator_2.id,
+            'operator_db_name': DB_TEMPLATE_2,
+        })
+
+        self.build_post_init_line_1 = self.env['build.post_init.line'].create({
             'key': 'mail_message',
             'value': 'mail.message'
         })
+        self.build_post_init_line_2 = self.env['build.post_init.line'].create({})
 
     def test_template_operator(self):
         # FIXME: that check needed when last tests didn't pass, not sure that it is correct way to drop db
-        if DB_TEMPLATE in db.list_dbs():
-            db.exp_drop(DB_TEMPLATE)
+        if DB_TEMPLATE_1 in db.list_dbs():
+            db.exp_drop(DB_TEMPLATE_1)
+        if DB_TEMPLATE_2 in db.list_dbs():
+            db.exp_drop(DB_TEMPLATE_2)
 
-        self.saas_template_operator.preparing_template_next()
+        # Template 1
+        self.saas_template_operator_1.preparing_template_next()
         # Tests that template db created correctly
-        self.assertTrue(self.saas_template_operator.operator_db_id.name)
-        self.assertEqual(self.saas_template_operator.operator_db_id.name, DB_TEMPLATE)
-        self.assertIn(DB_TEMPLATE, db.list_dbs())
-        self.assert_no_error_in_db(DB_TEMPLATE)
+        self.assertTrue(self.saas_template_operator_1.operator_db_id.name)
+        self.assertEqual(self.saas_template_operator_1.operator_db_id.name, DB_TEMPLATE_1)
+        self.assertIn(DB_TEMPLATE_1, db.list_dbs())
+        self.assert_no_error_in_db(DB_TEMPLATE_1)
 
         # Check that module from template_modules_domain is installed
-        self.assert_modules_is_installed(DB_TEMPLATE, MODULES)
-        self.assert_record_is_created(DB_TEMPLATE, 'mail.message', [('subject', '=', TEMPLATE_TEST_SUBJECT)])
+        self.assert_modules_is_installed(DB_TEMPLATE_1, MODULES)
+        self.assert_record_is_created(DB_TEMPLATE_1, 'mail.message', [('subject', '=', TEMPLATE_TEST_SUBJECT)])
+
+        # Template 2. Actually second template is done by calling preparing_template_next first time but that call
+        # is needed to recreate calling by crone every few minutes
+        self.saas_template_operator_2.preparing_template_next()
+
+        # Tests that template db created correctly
+        self.assertTrue(self.saas_template_operator_2.operator_db_id.name)
+        self.assertEqual(self.saas_template_operator_2.operator_db_id.name, DB_TEMPLATE_2)
+        self.assertIn(DB_TEMPLATE_2, db.list_dbs())
+        self.assert_no_error_in_db(DB_TEMPLATE_2)
+
+        # Check that module from template_modules_domain is installed
+        self.assert_modules_is_installed(DB_TEMPLATE_2, MODULES)
+        self.assert_record_is_created(DB_TEMPLATE_2, 'mail.message', [('subject', '=', TEMPLATE_TEST_SUBJECT)])
 
         # Check that database instance created correctly
-        if DB_INSTANCE in db.list_dbs():
-            db.exp_drop(DB_INSTANCE)
-        self.saas_template_operator.create_db(DB_INSTANCE, self.build_post_init_line)
-        self.assertIn(DB_INSTANCE, db.list_dbs())
-        self.assert_no_error_in_db(DB_INSTANCE)
-        self.assert_record_is_created(DB_INSTANCE, 'ir.config_parameter', [('key', '=', 'auth_quick.master')])
-        self.assert_record_is_created(DB_INSTANCE, 'ir.config_parameter', [('key', '=', 'auth_quick.build')])
-        self.assert_record_is_created(DB_INSTANCE, 'mail.message', [('subject', '=', BUILD_TEST_SUBJECT)])
+        if DB_INSTANCE_1 in db.list_dbs():
+            db.exp_drop(DB_INSTANCE_1)
+        if DB_INSTANCE_2 in db.list_dbs():
+            db.exp_drop(DB_INSTANCE_2)
+
+        self.saas_template_operator_1.create_db(DB_INSTANCE_1, self.build_post_init_line_1)
+        self.assertIn(DB_INSTANCE_1, db.list_dbs())
+        self.assert_no_error_in_db(DB_INSTANCE_1)
+        self.assert_record_is_created(DB_INSTANCE_1, 'ir.config_parameter', [('key', '=', 'auth_quick.master')])
+        self.assert_record_is_created(DB_INSTANCE_1, 'ir.config_parameter', [('key', '=', 'auth_quick.build')])
+        self.assert_record_is_created(DB_INSTANCE_1, 'mail.message', [('subject', '=', BUILD_TEST_SUBJECT)])
+
+        self.saas_template_operator_2.create_db(DB_INSTANCE_2, self.build_post_init_line_2)
+        self.assertIn(DB_INSTANCE_2, db.list_dbs())
+        self.assert_no_error_in_db(DB_INSTANCE_2)
+        self.assert_record_is_created(DB_INSTANCE_2, 'ir.config_parameter', [('key', '=', 'auth_quick.master')])
+        self.assert_record_is_created(DB_INSTANCE_2, 'ir.config_parameter', [('key', '=', 'auth_quick.build')])
