3d850e0947f19c15d2db1d7974ecbfb453785a23
Denis Mudarisov <mudarisov@it-projects.info>
2019-05-20 18:40:42 +0500

    :cat: add demo instead of creating records in tests

--- saas_demo/tests/test_saas_demo.py
+++ saas_demo/tests/test_saas_demo.py
@@ -13,31 +13,7 @@ class TestSaasDemo(TransactionCase):
             self.env.context,
             test_queue_job_no_delay=True,
         ))
-        self.saas_demo = self.env['saas.demo'].create({
-            'name': 'SaaS Demo Test'
-        })
-
-        self.saas_operator = self.env['saas.operator'].create({
-            'type': 'local',
-            'demo_id': self.saas_demo.id,
-            'master_url': 'http://saas_demo.127.0.0.1.nip.io:8069',
-        })
-
-        self.saas_module = self.env['saas.module'].create({
-            'name': 'web_login_background',
-        })
-
-        self.saas_template = self.env['saas.template'].create({
-            'demo_id': self.saas_demo.id,
-            'demo_main_addon_id': self.saas_module.id,
-            'demo_branch': '12.0',
-        })
-
-        self.saas_demo_repo = self.env['saas.demo.repo'].create({
-            'demo_id': self.saas_demo.id,
-            'url': 'https://gitlab.com/itpp/dev/saas-demo-test.git',
-            'branch': '12.0',
-        })
+        self.saas_demo = self.env.ref('saas_demo.saas_demo')
 
     def test_saas_demo(self):
         self.saas_demo.fetch_and_generate_templates()
