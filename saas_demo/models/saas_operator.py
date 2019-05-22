c7b1af04443e8863bb1bebad8264f96db1369e6c
Denis Mudarisov <mudarisov@it-projects.info>
2019-05-22 17:56:38 +0500

    :bath: update addons path only for same instance server

--- saas_demo/models/saas_operator.py
+++ saas_demo/models/saas_operator.py
@@ -1,9 +1,11 @@
 # Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 import logging
+import os
 
-from odoo import models, fields, api, service
+from odoo import models, fields, api, service, tools
 from ..os import repos_dir, update_addons_path, root_odoo_path, git
+from odoo.modules.module import ad_paths
 
 _logger = logging.getLogger(__name__)
 
@@ -85,3 +87,12 @@ class SAASOperator(models.Model):
         if self.type != 'local':
             return
         return self.demo_id.repo_ids._local_update_repo()
+
+    @api.multi
+    def update_ad_paths(self):
+        self.ensure_one()
+        if self.type != 'local':
+            return
+        path = os.path.join(tools.config['data_dir'], 'repos')
+        if path not in ad_paths:
+            return ad_paths.append(path)
