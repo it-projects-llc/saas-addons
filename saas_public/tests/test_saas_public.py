a3e9ac18027f013eef2dd26ad5e29c8a453d86bb
Denis Mudarisov <mudarisov@it-projects.info>
2019-07-09 17:15:09 +0500

    :fire: clarify tests

--- saas_public/tests/test_saas_public.py
+++ saas_public/tests/test_saas_public.py
@@ -15,6 +15,8 @@ class TestSaasPublic(HttpCase, Common):
         self.saas_template_1.write({
             'public_access': True,
         })
+        self.public_template = self.saas_template_1
+        self.private_template = self.saas_template_2
         self.saas_operator_1.write({
             'db_name_template': 'test_fast_build_{unique_id}',
         })
@@ -26,16 +28,14 @@ class TestSaasPublic(HttpCase, Common):
         drop_db_list = [build for build in db.list_dbs() if build.startswith('test_fast_build')]
         self.drop_dbs(drop_db_list)
         self.env['saas.template.operator'].preparing_template_next()
-        public_template_id = self.saas_template_1.id
         params = urllib.parse.urlencode({
             'mail_message': 'mail.message',
         })
-        url_1 = '/saas_public/{template_id}/create-fast-build?{params}'.format(template_id=public_template_id, params=params)
-        public_template = self.url_open(url_1.format(public_template_id))
-        self.assertIn(public_template.status_code, [200, 204], 'User must be redirected to the build')
+        url_1 = '/saas_public/{template_id}/create-fast-build?{params}'.format(template_id=self.public_template.id, params=params)
+        public_template_response = self.url_open(url_1)
+        self.assertIn(public_template_response.status_code, [200, 204], 'User must be redirected to the build')
         created_builds = [build for build in db.list_dbs() if build.startswith('test_fast_build')]
         self.assertTrue(created_builds)
-        private_template_id = self.saas_operator_2.id
-        url_2 = '/saas_public/{}/create-fast-build'
-        private_template = self.url_open(url_2.format(private_template_id))
+        url_2 = '/saas_public/{}/create-fast-build'.format(self.private_template.id)
+        private_template = self.url_open(url_2)
         self.assertIn(private_template.status_code, [404, 403], 'User should not have access to a private template')
