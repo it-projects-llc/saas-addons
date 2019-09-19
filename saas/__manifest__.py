5a3e967db6d95d51c1e927e26e4d8dfb68732617
Denis Mudarisov <mudarisov@it-projects.info>
2019-09-19 13:55:34 +0500

    :sparkles: don't use RPC on preparing template database (#61)

--- saas/__manifest__.py
+++ saas/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "SaaS",
     # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=12.0",
     "images": [],
-    "version": "12.0.1.0.3",
+    "version": "12.0.2.0.0",
     "application": False,
 
     "author": "IT-Projects LLC, Ivan Yelizariev",
