e9d994fa99d2496fef8c90745a0aa61a1c8c50bc
Denis Mudarisov <mudarisov@it-projects.info>
2019-09-26 13:24:03 +0500

    :bath: no code duplicates

--- saas_demo/controllers/saas_demo.py
+++ saas_demo/controllers/saas_demo.py
@@ -1,10 +1,11 @@
 # Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # Copyright 2019 Denis Mudarisov <https://it-projects.info/team/trojikman>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo.http import route, request, Controller
+from odoo.addons.saas_public.controllers.saas_public import SaaSPublicController
+from odoo.http import route, request
 
 
-class SaasDemoController(Controller):
+class SaasDemoController(SaaSPublicController):
     @route('/demo/<string:vendor>/<string:repo>/<string:branch>/<string:module>', type='http', auth='public')
     def create_demo_build(self, vendor, repo, branch, module, **kwargs):
         if not kwargs:
@@ -15,11 +16,5 @@ class SaasDemoController(Controller):
             ('repo_id.vendor', '=', vendor),
             ('repo_id.repo_name', '=', repo)
         ], limit=1)
-        if template_id and template_id.public_access:
-            template_operator_id = template_id.operator_ids.random_ready_operator()
-            build = template_operator_id.create_db(kwargs, with_delay=False)
-            build_url = build.get_url()
-            return request.env['auth_quick_master.token'].sudo().redirect_with_token(build_url, build.id,
-                                                                                     build_login='admin')
-        else:
-            return request.not_found()
+
+        return self._redirect_to_build(template_id, kwargs)
