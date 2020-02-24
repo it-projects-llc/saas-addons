f3b8e34f99bd2abf39d8b579ca9dc24c4588f2f3
Vildan Safin <safin@it-projects.info>
2020-02-24 19:00:21 +0500

    :bomb: developing process
    Signed-off-by: Vildan Safin <safin@it-projects.info>

--- saas_apps/controllers/main.py
+++ saas_apps/controllers/main.py
@@ -2,9 +2,40 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.http import route, request, Controller
-
+from odoo import http
+from openerp.http import request
+import json
 
 class SaaSAppsController(Controller):
-    @route('/price', type='http', auth='public')
-    def index(self, *kw):
-        return "shit"
+    @route('/price', auth='public', website=True)
+    def user_page(self, **kw):
+        import wdb
+        wdb.set_trace()
+        apps = http.request.env['saas.lines']
+        return http.request.render('saas_apps.index', {
+            'apps': apps.search([])
+        })
+
+    @route('/manage', auth='public', website=True)
+    def manager_page(self, **kw):
+        apps = http.request.env['ir.module.module']
+        return http.request.render('saas_apps.manage', {
+            'apps': apps.search([])
+        })
+    
+    # @route('/manage/<name>', auth='public', website=True)
+    # def module_information(self, name):
+    #     return http.request.render('saas_apps.info', {})
+    
+    @http.route(['/test'], type='json', auth='public', website=True)
+    def catch_app_click(self, **kw):
+        import wdb
+        wdb.set_trace()
+        # Mark choosen module as saleable
+        http.request.env['ir.module.module'].search([('name', '=', kw['args'][0])]).allow_to_sell = True
+        apps = http.request.env['saas.lines']
+        apps.add_new_module(kw['args'][0])
+        # request.redirect('/manage/%s' % name)
+        return {
+            'apps': kw['args'][0]
+        }
--- saas_apps/models/saas_apps.py
+++ saas_apps/models/saas_apps.py
@@ -2,10 +2,76 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
+import logging
 
+_logger = logging.getLogger(__name__)
 
-class SAASApps(models.Model):
-    _name = 'saas.apps'
+class SAASBasket(models.Model):
+    _name = 'saas.basket'
     _description = 'Module for selecting applications'
 
     users = fields.Integer(default=0)
+    module_sets_in_basket = fields.One2many('saas.set', 'basket', ondelete="cascade", delegate=True)
+    final_basket_price = fields.Float(default=0.0, compute='_compute_price', string="Price of the set")
+
+    def _compute_price(self):
+        for module_set in self.module_set_in_basket:
+            self.final_basket_price = self.final_basket_price + module_set.price
+
+
+class SAASLine(models.Model):
+    _name = 'saas.lines'
+    _description = 'Model line'
+
+    module_name = fields.Char(default="default")
+    price = fields.Float(default=0.0)
+    icon_path = fields.Char(compute='_compute_path', string="icon path")
+    dependencies = fields.Many2one('saas.set', string="Module dependences")
+    
+    def _compute_path(self): 
+        self.icon_path = "/saas_apps/static/src/img/%s.png" % self.module_name
+    
+    @api.constrains('price')
+    def _validate_price(self):
+        if self.price < 0:
+            self.price = 0
+            raise "Price can't be negative."
+
+    def add_new_module(self, name):
+        import wdb
+        wdb.set_trace()
+        # for module in self:
+        #     if(module.module_name == name)
+        #         return False
+        self.create({
+            'name': name
+        })
+        return True
+
+
+class SAASDependence(models.Model):
+    _name = 'saas.set'
+    _description = 'Module with dependencies'
+
+    basket = fields.Many2one('saas.basket', string='Modules in basket')
+    modules = fields.One2many('saas.lines', 'dependencies', ondelete='cascade', delegate=True)
+    final_set_price = fields.Float(default=0.0, compute='_compute_price', string="Price of the set")
+
+    def add_dependence(self, new_module_name, new_module_price):
+        try:
+            self.modules.create({
+                'module_name': new_module_name,
+                'price': int(new_module_name_price)
+            })
+        except:
+            _logger.error("Can't add new item in dependencies of this module")
+
+    def _compute_price(self):
+        for module in self.modules:
+            self.final_set_price = self.final_set_price + module.price
+
+
+# class IrModuleModule(models.Model):
+#     _inherit = "ir.module.module"
+
+#     allow_to_sell = fields.Boolean(default=False)
