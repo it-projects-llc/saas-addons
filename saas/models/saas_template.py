c68fa98b80f142b8e41ec0c103ca6fd5b9c5ceec
Eugene Molotov <molotov@it-projects.info>
2020-03-13 15:09:09 +0500

    :arrow_up::one::three: saas

--- saas/models/saas_template.py
+++ saas/models/saas_template.py
@@ -177,6 +177,7 @@ class SAASTemplateLine(models.Model):
             r.write({
                 'state': 'creating',
             })
+            r.flush()
             r.operator_db_id.with_delay().create_db(
                 None,
                 r.template_id.template_demo,
