b038cd6c5ccff9a1f4850f2fd13a564cfcd0a08b
Vildan Safin <safin@it-projects.info>
2020-04-07 18:04:59 +0500

    :bomb: Added saas.line price changing
    Signed-off-by: Vildan Safin <safin@it-projects.info>

--- saas_apps/controllers/main.py
+++ saas_apps/controllers/main.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.http import route, request, Controller
-from odoo import http
 from odoo.addons.saas_public.controllers.saas_public import SaaSPublicController
 import urllib.parse
 
@@ -13,54 +12,54 @@ class SaaSAppsController(Controller):
 
     @route('/price', type='http', auth='public', website=True)
     def user_page(self, **kw):
-        apps = http.request.env['saas.line'].sudo()
-        return http.request.render('saas_apps.index', {
+        apps = request.env['saas.line'].sudo()
+        return request.render('saas_apps.index', {
             'apps': apps.search([('allow_to_sell', '=', True)])
         })
 
-    @http.route(['/refresh'], type='json', auth='public')
+    @route(['/refresh'], type='json', auth='public')
     def catch_app_click(self, **kw):
-        apps = http.request.env['saas.line']
+        apps = request.env['saas.line']
         apps.refresh_lines()
         return {}
 
-    @http.route(['/what_dependencies'], type='json', auth='public')
-    def what_dependencies(self, **kw):
-        app_tech_name = kw['args'][0]
-        app = http.request.env['saas.line'].sudo().search([('name', '=', app_tech_name)])
+    @route(['/what_dependencies'], type='json', auth='public')
+    def search_incoming_app_dependencies(self, **kw):
+        app_tech_name = kw['root'][0]
+        app = request.env['saas.line'].sudo().search([('name', '=', app_tech_name)])
         return {
             'dependencies': app.dependencies_info('root')
         }
 
-    # @http.route(['/what_dependencies_optimized'], type='json', auth='public')
+    # @route(['/what_dependencies_optimized'], type='json', auth='public')
     # def what_dependencies_optimized(self, **kw):
     #     apps = []
-    #     for app_name in kw['args'][0]:
-    #         app = http.request.env['saas.line'].sudo().search([('name', '=', app_name)])
+    #     for app_name in kw['root'][0]:
+    #         app = request.env['saas.line'].sudo().search([('name', '=', app_name)])
     #         apps.append({app_name: app.dependencies_info('root')})
     #     return {
     #         'dependencies': apps
     #     }
 
 class SaaSAppsPublicController(SaaSPublicController):
-    @http.route(['/create_saas_template'], type='json', auth='public', website=True)
+    @route(['/create_saas_template'], type='json', auth='public', website=True)
     def create_saas_template(self, **kw):
-        templates = http.request.env['saas.template']
-        installing_modules_names = kw['args'][0]
+        templates = request.env['saas.template']
+        installing_modules_names = kw.get('module_names', [])
         saas_template = templates.sudo().create({
-            'name': 'Template ' + str(len(templates.sudo().search([])) - 1),
+            'name': 'Template ' + str(templates.sudo().search_count([]) - 1),
             'template_demo': True,
             'public_access': True
         })
         for name in installing_modules_names:
-            saas_template.template_module_ids += http.request.env['saas.module'].sudo().search([('name', '=', name)])
-        saas_operator = http.request.env['saas.operator'].sudo().search([('db_url_template', '=', 'http://{db_name}.{db_id}.127.0.0.1.nip.io')])
-        saas_template_operator = http.request.env['saas.template.operator'].sudo().create({
+            saas_template.template_module_ids += request.env['saas.module'].sudo().search([('name', '=', name)])
+        saas_operator = request.env['saas.operator'].sudo().search([('db_url_template', '=', 'http://{db_name}.{db_id}.127.0.0.1.nip.io')])
+        saas_template_operator = request.env['saas.template.operator'].sudo().create({
             'template_id': saas_template.id,
             'operator_id': saas_operator.id,
-            'operator_db_name': DB_TEMPLATE + str(len(http.request.env['saas.template.operator'].sudo().search([])) + 1),
+            'operator_db_name': DB_TEMPLATE + str(request.env['saas.template.operator'].sudo().search_count([]) + 1),
         })
-        http.request.env['saas.template.operator'].sudo().preparing_template_next()
+        request.env['saas.template.operator'].sudo().preparing_template_next()
         # message = '''Template\'s deployment with name {} is creating
         # and will be ready in a few minutes.'''.format(r.operator_db_name)
         # self.operator_id.notify_users(message, message_type='info')
@@ -72,10 +71,10 @@ class SaaSAppsPublicController(SaaSPublicController):
             'state': 'Database creating...'
         }
 
-    @http.route(['/check_saas_template'], type='json', auth='public', website=True)
+    @route(['/check_saas_template'], type='json', auth='public', website=True)
     def is_build_created(self, **kw):
-        template = http.request.env['saas.template'].sudo().browse(kw['args'][0]['template'])
-        template_operator = http.request.env['saas.template.operator'].sudo().browse(kw['args'][0]['template_operator'])
+        template = request.env['saas.template'].sudo().browse(kw['templates'][0]['template'])
+        template_operator = request.env['saas.template.operator'].sudo().browse(kw['templates'][0]['template_operator'])
         if len(template) > 0 and template_operator.random_ready_operator_check():
             kwargs = {}
             if template and template.public_access:
@@ -93,14 +92,13 @@ class SaaSAppsPublicController(SaaSPublicController):
                     'Error': '0',
                     'state': template_operator.state
                 }
-            else:
-                return {
-                    'template': '0',
-                    'template_operator': '0',
-                    'link': '0',
-                    'Error': '1',
-                    'state': template_operator.state
-                }
+            return {
+                'template': '0',
+                'template_operator': '0',
+                'link': '0',
+                'Error': '1',
+                'state': template_operator.state
+            }
         else:
             return {
                 'template': template.id,
