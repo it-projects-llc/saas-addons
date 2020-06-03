ce3a761e586317fa67ca74b28093ea13f71f3b73
Eugene Molotov <molotov@it-projects.info>
2020-06-03 10:31:54 +0500

    saas_product: make sure no taxes are used, when creating saas product

--- saas_product/models/product_template.py
+++ saas_product/models/product_template.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
 # License MIT (https://opensource.org/licenses/MIT).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -9,3 +9,10 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     is_saas_product = fields.Boolean("Is SaaS product?", default=False)
+
+    @api.model
+    def create(self, vals):
+        if vals.get("is_saas_product"):
+            vals["taxes_id"] = [(5,)]
+            vals["supplier_taxes_id"] = [(5,)]
+        return super(ProductTemplate, self).create(vals)
