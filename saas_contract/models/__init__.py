a71286c7687b3e0f89bd706bae15956c818a0b0a
Eugene Molotov <molotov@it-projects.info>
2020-07-28 11:57:08 +0500

    saas_contract: recomputing is_paid after running _compute_amount in account move

--- saas_contract/models/__init__.py
+++ saas_contract/models/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from . import account_move
 from . import saas_db
 from . import contract_contract
 from . import contract_line
