3f6ab3b5de16ea06bc8e07afdc7f4d9f2a3a0c61
Eugene Molotov <molotov@it-projects.info>
2020-08-07 13:08:43 +0500

    saas_contract: добавил поле build_status

--- saas_contract/models/contract_contract.py
+++ saas_contract/models/contract_contract.py
@@ -11,13 +11,18 @@ class Contract(models.Model):
     # TODO: запретить ситуацию, где набор линии в контракте, в котором есть пустое пространство из будущего времени (хотя-бы в плане юзеров)
     build_id = fields.Many2one("saas.db", readonly=True)
     build_expiration_date_defacto = fields.Datetime("Build expiration date (defacto)", related="build_id.expiration_date")
+    build_status = fields.Selection([
+        ("trial", "Trial"),
+        ("active", "Active"),
+        ("suspended", "Suspended"),
+    ], "Build status", readonly=True)
 
     def write(self, vals):
         res = super(Contract, self).write(vals)
         self.mapped("contract_line_ids")._recompute_is_paid()
         return res
 
-    @api.depends("contract_line_ids.is_paid")
+    @api.depends("contract_line_ids", "contract_line_ids.is_paid", "build_id")
     def action_update_build(self):
         for contract in self.filtered("build_id"):
             build = contract.build_id
@@ -28,6 +33,12 @@ class Contract(models.Model):
                 and line.date_start <= fields.Date.context_today(line) <= line.date_end
             ).mapped("quantity")
 
+            is_trial = bool(contract.contract_line_ids.filtered(
+                lambda line: line.product_id == self.env.ref("saas_product.product_users_trial")
+                and line.is_paid
+                and line.date_start <= fields.Date.context_today(line) <= line.date_end
+            ))
+
             build_expiration_date = max(
                 contract.contract_line_ids
                 .filtered(lambda line: line.product_id.product_tmpl_id == self.env.ref("saas_product.product_users") and line.is_paid)
@@ -37,6 +48,10 @@ class Contract(models.Model):
             build.write({
                 "expiration_date": build_expiration_date,
                 "max_users_limit": sum(max_users_limit) or 1,
+            })
+            # TODO: тут не будет повторный раз у билда считаться?
+            contract.write({
+                "build_status": is_trial and "trial" or (fields.Date.context_today(contract) <= build_expiration_date and "active" or "suspended")
             })
 
     def _action_update_all_builds(self):
