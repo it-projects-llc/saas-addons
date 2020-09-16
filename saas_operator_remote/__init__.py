ac7212e9a1bca05a9434d46ade33f9381dad2c3e
Eugene Molotov <molotov@it-projects.info>
2020-09-16 17:40:31 +0500

    saas_operator_remote: первая реализация

---
+++ saas_operator_remote/__init__.py
@@ -0,0 +1,4 @@
+# License MIT (https://opensource.org/licenses/MIT).
+
+from . import controllers
+from . import models
---
+++ saas_operator_remote/controllers/main.py
@@ -0,0 +1,94 @@
+# Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, http, tools, sql_db, SUPERUSER_ID, registry
+from odoo.http import request, route
+from odoo.service import db
+from odoo.http import _request_stack
+from odoo.exceptions import AccessError
+import functools
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+# TODO: add decorator for ensuring master password and returning exception
+def check_master_pwd(f):
+    @functools.wraps(f)
+    def wrap(*args, **kw):
+        master_pwd = kw.pop("master_pwd", None)
+        if not master_pwd:
+            raise AccessError("master password (master_pwd) not given")
+        if not tools.config.verify_admin_password(master_pwd):
+            raise AccessError("Incorrect master password or master password not set")
+        return f(*args, **kw)
+    return wrap
+
+
+class OperatorController(http.Controller):
+    @route("/saas_operator/create_db", type="json", auth="none")
+    @check_master_pwd
+    def create_db(self, template_db, db_name, demo, lang="en_US"):
+        # to avoid installing extra modules we need this condition
+        if tools.config["init"]:
+            tools.config["init"] = {}
+
+        # we don't need tests in templates and builds
+        test_enable = tools.config["test_enable"]
+        if test_enable:
+            tools.config["test_enable"] = {}
+
+        try:
+            if template_db:
+                # TODO: does drop connection works ok?
+                with sql_db.db_connect(template_db).cursor() as cr:
+                    db._drop_conn(cr, template_db)
+                db.exp_duplicate_database(template_db, db_name)
+            else:
+                db.exp_create_database(db_name, demo, lang)
+
+        finally:
+            if test_enable:
+                tools.config["test_enable"] = test_enable
+
+    @route("/saas_operator/drop_db", type="json", auth="none")
+    @check_master_pwd
+    def drop_db(self, db_name):
+        db.exp_drop(db_name)
+
+    @route("/saas_operator/install_modules", type="json", auth="none")
+    @check_master_pwd
+    def install_modules(self, db_name, modules):
+        db = sql_db.db_connect(db_name)
+        with api.Environment.manage(), db.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+
+            # Set odoo.http.request to None.
+            #
+            # Odoo tries to use its values in translation system, which may eventually
+            # change currentThread().dbname to saas master value.
+            _request_stack.push(None)
+
+            module_ids = env['ir.module.module'].search([('state', '=', 'uninstalled')] + modules)
+            module_ids.button_immediate_install()
+
+            # Some magic to force reloading registry in other workers
+            env.registry.registry_invalidated = True
+            env.registry.signal_changes()
+
+            # return request back
+            _request_stack.pop()
+
+    @route("/saas_operator/post_init", type="json", auth="none")
+    @check_master_pwd
+    def post_init(self, db_name, template_post_init):
+        registry(db_name).check_signaling()
+        with api.Environment.manage(), db.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            action = env['ir.actions.server'].create({
+                'name': 'Local Code Eval',
+                'state': 'code',
+                'model_id': 1,
+                'code': template_post_init
+            })
+            action.run()
