57a9aaa442076320e0ce78c1ec74c8d4d6ed26af
Eugene Molotov <molotov@it-projects.info>
2020-09-17 18:28:20 +0500

    saas_operator_remote: теперь знаем как хранить exception

--- saas_operator_remote/models/saas_operator.py
+++ saas_operator_remote/models/saas_operator.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
+from odoo.addons.queue_job.exception import FailedJobError
 import requests
 
 
@@ -11,9 +12,17 @@ def jsonrpc(url, params, timeout=1200):
         'params': params
     }
     req = requests.post(url, json=data, timeout=timeout)
-    print(req.content)
     req.raise_for_status()
-    return req.json()
+    try:
+        response = req.json()
+    except ValueError:
+        raise FailedJobError(req.text)
+
+    error = response.get("error")
+    if error:
+        raise FailedJobError(error.get("data", {}).get("debug"))
+
+    return response
 
 
 class SaasOperator(models.Model):
@@ -28,33 +37,23 @@ class SaasOperator(models.Model):
     def _create_db(self, template_db, db_name, demo, lang="en_US"):
         remote_ops = self.filtered(lambda op: op.type == "remote")
         for op in remote_ops:
-            result = jsonrpc(op.global_url + "/saas_operator/create_db", {
+            jsonrpc(op.global_url + "/saas_operator/create_db", {
                 "master_pwd": op.master_pwd,
                 "template_db": template_db,
                 "db_name": db_name,
                 "demo": demo,
                 "lang": lang,
             })
-            # TODO: get exceptions
-            error = result.get("error")
-            if error:
-                print("FUCK")
-                import wdb; wdb.set_trace()
 
         super(SaasOperator, self - remote_ops)._create_db(template_db, db_name, demo, lang)
 
     def _drop_db(self, db_name):
         remote_ops = self.filtered(lambda op: op.type == "remote")
         for op in remote_ops:
-            result = jsonrpc(op.global_url + "/saas_operator/drop_db", {
+            jsonrpc(op.global_url + "/saas_operator/drop_db", {
                 "master_pwd": op.master_pwd,
                 "db_name": db_name,
             })
-            # TODO: get exceptions
-            error = result.get("error")
-            if error:
-                print("FUCK")
-                import wdb; wdb.set_trace()
 
         super(SaasOperator, self - remote_ops)._drop_db(db_name)
 
@@ -62,28 +61,18 @@ class SaasOperator(models.Model):
         if self.type != "remote":
             return super(SaasOperator, self)._install_modules(db_name, modules)
 
-        result = jsonrpc(self.global_url + "/saas_operator/install_modules", {
+        jsonrpc(self.global_url + "/saas_operator/install_modules", {
             "master_pwd": self.master_pwd,
             "db_name": db_name,
             "modules": modules,
         })
-        # TODO: get exceptions
-        error = result.get("error")
-        if error:
-            print("FUCK")
-            import wdb; wdb.set_trace()
 
     def _post_init(self, db_name, template_post_init):
         if self.type != "remote":
             return super(SaasOperator, self)._post_init(db_name, template_post_init)
 
-        result = jsonrpc(self.global_url + "/saas_operator/post_init", {
+        jsonrpc(self.global_url + "/saas_operator/post_init", {
             "master_pwd": self.master_pwd,
             "db_name": db_name,
             "template_post_init": template_post_init,
         })
-        # TODO: get exceptions
-        error = result.get("error")
-        if error:
-            print("FUCK")
-            import wdb; wdb.set_trace()
