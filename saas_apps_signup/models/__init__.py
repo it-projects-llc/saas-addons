7832b19ec431ff8523a59a3109a0e9fdcd6f2728
Eugene Molotov <molotov@it-projects.info>
2020-09-24 11:47:45 +0500

    saas_apps_signup -> saas_master_custom_hesham: вынес специфичную часть в отдельный модуль

--- saas_apps_signup/models/__init__.py
+++ saas_apps_signup/models/__init__.py
@@ -5,7 +5,6 @@ from . import saas_db
 from . import contract_contract
 from . import res_partner
 from . import account_move
-from . import saas_operator
 from . import res_config_settings
 from . import sale_order
 from . import mail_thread
---
+++ saas_master_custom_hesham/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "SaaS: Master (Hesham Edition)",
+    "version": "13.0.1.0.0",
+    "depends": ["saas"],
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "data": [],
+    "qweb": [],
+    "demo": [],
+    "license": "AGPL-3",
+}
