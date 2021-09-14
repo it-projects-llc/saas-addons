a57ada7fb8739ec49eb795b21f9055816faba2c6
Eugene Molotov <molotov@it-projects.info>
2021-09-14 11:30:23 +0500

    :tada: host2db

---
+++ host2db/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
+# License MIT (https://opensource.org/licenses/MIT).
+
+{
+    "name": """Convert Host to db""",
+    "images": [],
+    "version": "1.0.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "license": "Other OSI approved licence",  # MIT
+    "depends": [],
+    "external_dependencies": {"python": [], "bin": []},
+    "installable": False,
+}
