f5bbd52dad429e88465d1253f033275bff9cd0ae
Eugene Molotov <molotov@it-projects.info>
2021-10-04 16:09:21 +0500

    :heart_eyes: saas_expiration: added ability to set default number of days until expiration

--- saas_expiration/doc/changelog.rst
+++ saas_expiration/doc/changelog.rst
@@ -1,3 +1,8 @@
+`1.0.1`
+-------
+
+- **Improvement:** added ability to set default number of days until expiration
+
 `1.0.0`
 -------
 
--- saas_expiration/models/saas_db.py
+++ saas_expiration/models/saas_db.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT
 from datetime import datetime, timedelta
 
@@ -10,9 +10,20 @@ class SaasDb(models.Model):
 
     _inherit = "saas.db"
 
+    @api.model
+    def _get_number_of_days_for_trial(self):
+        return int(
+            self.env["ir.config_parameter"].get_param(
+                "saas_expiration.number_of_days_for_trial", 7
+            )
+        )
+
+    # fmt: off
     expiration_date = fields.Datetime(
-        "Expiration date", default=lambda *args: datetime.now() + timedelta(days=7)
+        "Expiration date",
+        default=lambda self: datetime.now() + timedelta(days=self._get_number_of_days_for_trial()),
     )
+    # fmt: on
 
     def write_values_to_build(self):
         super(SaasDb, self).write_values_to_build()
